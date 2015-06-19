49c55b1ccf02c3392d25ca111e1dbbdd12009db2
bcollins <bcollins@blueraster.com>
2015-06-19 02:40:56 -0400

    started osx setup script

---
+++ setup_osx.sh
@@ -0,0 +1,7 @@
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install vim
+brew install tmux
+brew install zsh
+brew install wget
+brew install macvim
+brew install ssh-copy-id
