77145f63fe0cf3c4f67b88fa1b8fc30617b5da35
bcollins <bcollins@blueraster.com>
2015-06-19 11:39:15 -0400

    added supertab

--- setup_vim.sh
+++ setup_vim.sh
@@ -6,4 +6,5 @@ git clone git@github.com:kien/rainbow_parentheses.vim.git
 git clone git@github.com:nvie/vim-flake8.git
 git clone https://github.com/scrooloose/nerdtree.git
 git clone git@github.com:altercation/vim-colors-solarized.git
+git clone git@github.com:ervandew/supertab.git
 cd -
