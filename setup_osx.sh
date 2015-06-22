3fa8c46b3e48c8888f286a936a6f9131f5a33c60
bcollins <parietal.io@gmail.com>
2015-06-22 17:16:46 -0400

    added ohmyzsh setup lines

--- setup_osx.sh
+++ setup_osx.sh
@@ -1,4 +1,5 @@
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 brew install vim
 brew install tmux
 brew install zsh
